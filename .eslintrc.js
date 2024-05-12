cf446746336c554b479a0b26fe40661c5d84e94e
1F10230014 <s1f102300144@iniad.org>
2024-05-12 19:32:34 +0900

    add:plugin:@react-three

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,14 @@ module.exports = {
   root: true,
   extends: [
     'eslint:recommended',
+    'plugin:@react-three',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'prettier',
   ],
-  plugins: ['@typescript-eslint', 'react'],
+  plugins: ['@typescript-eslint', 'react', '@react-three'],
   parser: '@typescript-eslint/parser',
   env: {
     browser: true,
