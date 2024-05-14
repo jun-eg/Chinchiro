05cd3f65d5f404bfedc6a2a0f74c22070025bf12
1F10230014 <s1f102300144@iniad.org>
2024-05-14 22:46:12 +0900

    update:出目、振り数表示追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,48 @@ export type AnimationState = 'rest' | 'rolling' | 'drop';
 const Home = () => {
   const [dicevalues, setDiceValues] = useState<number[]>([1, 1, 1]);
   const [animatinState, setAnimationState] = useState<AnimationState>('rest');
+  const [playCount, setPlayCount] = useState<number>(0);
+  const [gameResult, setGameresult] = useState<number>();
+  let newDiceValues: [number, number, number] = [0, 0, 0];
+
+  const count = () => {
+    const newPlayCount = (playCount + 1) % 4;
+    setPlayCount(newPlayCount);
+  };
 
   const sleep = (ms: number): Promise<void> => {
     return new Promise((resolve) => {
       setTimeout(resolve, ms);
     });
+  };
+
+  const confirmResult = (oneDiceVqalu: number, twoDiceValu: number, threeDiceValue: number) => {
+    if (oneDiceVqalu === twoDiceValu) {
+      setPlayCount(3);
+      return threeDiceValue;
+    }
+    if (oneDiceVqalu === threeDiceValue) {
+      setPlayCount(3);
+      return twoDiceValu;
+    }
+    if (twoDiceValu === threeDiceValue) {
+      setPlayCount(3);
+      return oneDiceVqalu;
+    }
+    return 0;
+  };
+
+  const onClickDice = (): { newDiceValues: number[] } => {
+    setGameresult(0);
+    newDiceValues = [
+      Math.floor(Math.random() * 6 + 1),
+      Math.floor(Math.random() * 6 + 1),
+      Math.floor(Math.random() * 6 + 1),
+    ];
+
+    count();
+    setDiceValues(newDiceValues);
+    return { newDiceValues };
   };
 
   const startAnimation = async (): Promise<void> => {
@@ -21,22 +58,14 @@ const Home = () => {
     setAnimationState('drop');
     await sleep(500);
     setAnimationState('rest');
-  };
-
-  const onClickDice = (): { newDiceValues: number[] } => {
-    const newDiceValues: number[] = [
-      Math.floor(Math.random() * 6 + 1),
-      Math.floor(Math.random() * 6 + 1),
-      Math.floor(Math.random() * 6 + 1),
-    ];
-    setDiceValues(newDiceValues);
-
-    return { newDiceValues };
+    setGameresult(confirmResult(newDiceValues[0], newDiceValues[1], newDiceValues[2]));
   };
 
   return (
     <>
       <div className={styles.container}>
+        <div>振り数:{playCount}</div>
+        <div>出目:{gameResult}</div>
         <Canvas>
           <ambientLight intensity={1.5} />
           <pointLight position={[0, 10, 0]} intensity={200} />
